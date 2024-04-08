59c8b21eb96cf1f3e06045f263a6aa2ed511496e
Jake Strasler <straslerj@msoe.edu>
2024-04-08 15:32:41 -0500

    Creates front end service

---
+++ services/frontend/frontend.py
@@ -0,0 +1,85 @@
+import configparser
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import pymongo
+import os
+import re
+
+from flask import Flask, render_template
+
+
+"""
+Prediction service which pulls in a model from an S3 bucket, makes predictions on data
+from the database created by the scraper service, and then uploads the predictions to 
+a new databse collection to be displayed on the frontend.
+"""
+config = configparser.ConfigParser()
+config.read("config.ini")
+URI = config["mongodb"]["uri"]
+DB_NAME = config["mongodb"]["db_name"]
+SCRAPER_COLLECTION = config["mongodb"]["scraper_collection"]
+PREDICTION_COLLECTION = config["mongodb"]["prediction_collection"]
+ACCESS_KEY = config["tebi"]["access_key"]
+SECRET_KEY = config["tebi"]["secret_key"]
+ENDPOINT_URL = config["tebi"]["endpoint_url"]
+BUCKET_NAME = config["tebi"]["bucket_name"]
+LOCAL_MODEL_NAME = "model.pkl"
+
+app = Flask(__name__)
+
+
+def connect_to_mongodb() -> pymongo.MongoClient:
+    """Makes connection to MongoDB
+
+    Returns:
+        MongoClient: Instance of MongoDB
+    """
+
+    client = pymongo.MongoClient(URI)
+
+    db = client[DB_NAME]
+    return db
+
+
+def prepare_for_table(input_dict):
+    for key, value in input_dict.items():
+        if key in [
+            "max_weight",
+            "diameter",
+            "height",
+            "rim_depth",
+            "rim_thickness",
+            "inside_rim_diameter",
+            "rim_depth_diameter_ratio",
+            "flexibility",
+        ]:
+            try:
+                value = value.rstrip("grkcm%")
+                input_dict[key] = float(value)
+            except ValueError:
+                pass
+        if key in ["TURN"]:
+            try:
+                if value == -0.0:
+                    input_dict[key] = 0.0 - float(value)
+            except ValueError:
+                pass
+        if key in "approved_date":
+            print(value)
+            date_object = datetime.strptime(value, "%b %d, %Y").date()
+            input_dict[key] = date_object
+    return input_dict
+
+
+@app.route("/")
+def index():
+    db = connect_to_mongodb()
+    discs_cursor = db[PREDICTION_COLLECTION].find()
+    discs = list(discs_cursor)
+    processed_discs = [prepare_for_table(disc) for disc in discs]
+    return render_template("index.html", discs=processed_discs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8003)
