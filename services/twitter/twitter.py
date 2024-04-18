e2496ea34c385595b24209dfa420d07b20e1e32d
Jake Strasler <straslerj@msoe.edu>
2024-04-18 10:29:01 -0500

    Adds response time metric to Twitter

--- services/twitter/twitter.py
+++ services/twitter/twitter.py
@@ -78,7 +78,27 @@ def authenticate():
     )
 
 
-def write_usage_log(db, collection, endpoint, method, response_code, response_message):
+def write_usage_log(
+    db: pymongo.MongoClient,
+    collection: str,
+    endpoint: str,
+    method: str,
+    response_code: int,
+    response_message: str,
+    start_time: datetime.now,
+):
+    """Writes a log to MongoDB database
+
+    Args:
+        db (pymongo.MongoClient): the database to write the log to
+        collection (str): the colleciton within the database
+        endpoint (str): the endpoint called
+        method (str): the ReST method used to call the endpoint
+        response_code (int): the response code returned
+        response_message (str): the message returned
+        start_time (datetime): used to determine the response time of the endpoint
+    """
+    response_time = (datetime.now() - start_time).total_seconds() * 1000  # milliseconds
     db[collection].insert_one(
         {
             "endpoint": endpoint,
@@ -86,12 +106,15 @@ def write_usage_log(db, collection, endpoint, method, response_code, response_me
             "time": datetime.now(),
             "response_code": response_code,
             "response_message": response_message,
+            "response_time": response_time,
         }
     )
 
 
 @app.route("/create_tweet", methods=["POST"])
+@verify_api_key
 def create_tweet():
+    start_time = datetime.now()
     try:
         db = connect_to_mongodb()
         prediction_collection = db[PREDICTION_COLLECTION]
@@ -109,7 +132,9 @@ def create_tweet():
             new_tweets += 1
 
         message = f"{new_tweets} tweets created successfully."
-        write_usage_log(db, USAGE_COLLECTION, "/create_tweet", "POST", 200, message)
+        write_usage_log(
+            db, USAGE_COLLECTION, "/create_tweet", "POST", 200, message, start_time
+        )
         return (
             jsonify({"message": message}),
             200,
@@ -136,22 +161,29 @@ def admin():
                 "_id": "$endpoint",
                 "count": {"$sum": 1},
                 "last_run": {"$max": "$time"},
+                "average_time": {
+                    "$avg": "$response_time"
+                },  # Calculate average response time
             }
         },
     ]
 
     aggregation_result = list(collection.aggregate(pipeline))
 
-    endpoint_counts = {
-        item["_id"]: {"count": item["count"], "last_run": item["last_run"]}
+    endpoint_data = {
+        item["_id"]: {
+            "count": item["count"],
+            "last_run": item["last_run"],
+            "average_time": (
+                round(item["average_time"], 2) if item["average_time"] else 0
+            ),  # Round to 2 decimal places
+        }
         for item in aggregation_result
     }
 
     all_entries = list(collection.find({}, {"_id": 0}).sort("time", -1))
 
-    return render_template(
-        "admin.html", endpoint_counts=endpoint_counts, log=all_entries
-    )
+    return render_template("admin.html", endpoint_data=endpoint_data, log=all_entries)
 
 
 if __name__ == "__main__":
